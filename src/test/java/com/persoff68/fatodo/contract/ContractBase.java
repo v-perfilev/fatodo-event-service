a56c2e900281997e48fa992dcfc8913d5d201f00
Vladimir Perfilev <persoff68@gmail.com>
2022-07-23 18:40:50 +0200

    TDM-150 fix contact base

--- src/test/java/com/persoff68/fatodo/contract/ContractBase.java
+++ src/test/java/com/persoff68/fatodo/contract/ContractBase.java
@@ -10,6 +10,7 @@ import com.persoff68.fatodo.repository.EventRecipientRepository;
 import com.persoff68.fatodo.repository.EventRepository;
 import com.persoff68.fatodo.repository.ReadStatusRepository;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,7 +23,7 @@ import java.util.UUID;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMessageVerifier
-public abstract class ContractBase {
+class ContractBase {
 
     private static final String USER_ID = "8f9a7cae-73c8-4ad6-b135-5bd109b51d2e";
 
@@ -40,15 +41,18 @@ public abstract class ContractBase {
     WsServiceClient wsServiceClient;
 
     @BeforeEach
-    public void setup() {
+    void setup() {
         RestAssuredMockMvc.webAppContextSetup(context);
-
-        eventRepository.deleteAll();
-        eventRecipientRepository.deleteAll();
-        readStatusRepository.deleteAll();
 
         Event event = buildEvent(USER_ID);
         eventRepository.save(event);
+    }
+
+    @AfterEach
+    void cleanup() {
+        eventRepository.deleteAll();
+        eventRecipientRepository.deleteAll();
+        readStatusRepository.deleteAll();
     }
 
     private Event buildEvent(String userId) {

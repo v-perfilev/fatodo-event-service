d493d17976eb932941c9d02925acb87848fbc1d5
Vladimir Perfilev <persoff68@gmail.com>
2022-09-27 17:36:53 +0200

    TDM-156 fix tests

--- src/test/java/com/persoff68/fatodo/builder/event/TestChat.java
+++ src/test/java/com/persoff68/fatodo/builder/event/TestChat.java
@@ -1,26 +1,32 @@
 package com.persoff68.fatodo.builder.event;
 
 import com.persoff68.fatodo.model.event.Chat;
+import com.persoff68.fatodo.model.event.ChatMember;
 import lombok.Builder;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.UUID;
 
 public class TestChat extends Chat {
 
     @Builder
-    public TestChat(UUID id) {
+    public TestChat(UUID id, List<ChatMember> members) {
         super();
         super.setId(id);
+        super.setMembers(members);
     }
 
     public static TestChatBuilder defaultBuilder() {
         return TestChat.builder()
-                .id(UUID.randomUUID());
+                .id(UUID.randomUUID())
+                .members(Collections.emptyList());
     }
 
     public Chat toParent() {
         Chat chat = new Chat();
         chat.setId(getId());
+        chat.setMembers(getMembers());
         return chat;
     }
 
